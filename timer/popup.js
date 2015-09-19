eb568acd4add09d68ea473026393fd81c554700a
Evan Sandhoefner <evansandhoefner@college.harvard.edu>
2015-09-19 18:35:20 -0400

    spacebar and enter are great but numbers are giving NaN. note comes from page.js:11

--- timer/popup.js
+++ timer/popup.js
@@ -36,7 +36,9 @@ window.onkeyup = function(e) {
     if (e.keyCode == 32) {
       console.log("spacebar!");
     } else if (e.keyCode >= 48 && e.keyCode <= 57) {
-      console.log(e.keycode - 48);
+      console.log(e.keycode);
+      press = parseInt(e.keycode) - 48;
+      console.log(parseInt(press));
     } else if (e.keyCode == 13) {
       console.log('enter!');
     }
