0b4a224feefdb551387252e3e4d0409ec10f07d1
Evan Sandhoefner <evansandhoefner@college.harvard.edu>
2015-09-20 18:28:34 -0400

    option save works

--- timer/options.js
+++ timer/options.js
@@ -16,10 +16,10 @@ stats: stats
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    status.innerHTML = 'Options saved.';
     setTimeout(function() {
-      status.textContent = '';
-    }, 750);
+      status.innerHTML = '<br>';
+    }, 1024);
   });
 }
 
@@ -28,11 +28,19 @@ stats: stats
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
+    skins: false,
+names: false,
+colors: false,
+mass: true,
+theme: false,
+stats: false
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+    document.getElementById('No skins').checked = items.skins;
+    document.getElementById('No names').checked = items.names;
+    document.getElementById('No colors').checked = items.colors;
+    document.getElementById('Show mass').checked = items.mass;
+    document.getElementById('Dark theme').checked = items.dark;
+    document.getElementById('Skip stats').checked = items.stats;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
