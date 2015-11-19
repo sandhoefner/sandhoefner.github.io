cb57cda291ca40a94161a74cf356f424db9d3713
Evan Sandhoefner <evansandhoefner@college.harvard.edu>
2015-11-19 18:35:23 -0500

    viewpager works but it's weird so I'm not sure if I want it. can't figure out scaling, title bar

--- MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/Grades.java
+++ MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/Grades.java
@@ -1,43 +1,51 @@
 package com.mycompany.myfirstapp;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Grades extends AppCompatActivity {
+public class Grades extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grades);
         setTitle("Grade Conversions");
         SharedPreferences sp = getSharedPreferences("bg", 0);
         String hexaColor = sp.getString("hexa", "#FFFFFF"); //default color will be #FFFFFF
         getWindow().getDecorView().setBackgroundColor(Color.parseColor(hexaColor));
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this, imageArra);
+        ViewPager myPager = (ViewPager) findViewById(R.id.myfivepanelpager);
+        myPager.setAdapter(adapter);
+        myPager.setCurrentItem(0);
+
     }
+
+    private int imageArra[] = { R.drawable.boulder_grades, R.drawable.sport_grades};
 /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_grades, menu);
+        getMenuInflater().inflate(R.menu.activity_grades, menu);
         return true;
     }
-*/
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    */
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+        return true;
     }
+
+    return super.onOptionsItemSelected(item);
+}
+
 }
