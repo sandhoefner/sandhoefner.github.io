212bcb32a78fe3f15410c74b18bca22df244acd3
Evan Sandhoefner <evansandhoefner@college.harvard.edu>
2015-12-08 21:27:17 -0500

    this version is a bit fucked up. working on a way to smart-continue until end and then smart-terminate, without stack overflow. tricky.

--- nostalgia/page.js
+++ nostalgia/page.js
@@ -1,13 +1,41 @@
 window.onload = function() {
     console.log("going");
+    console.log(document.getElementsByClassName("touchable primary"));
     
     var button;
 
-    for (i=0; i<10000; i+=100) {
-        setTimeout(function(){
-            button = document.getElementsByClassName("touchable primary")[7];
-            console.log("click");
-            button.click();
-        }, i);
+    function flurry() {
+        for (i=0; i<=10000; i+=100) {
+            setTimeout(function(){
+                button = document.getElementsByClassName("touchable primary")[7];
+                console.log("click");
+                button.click();
+                if (i >= 10000) {
+                    console.log(i,unfinished);
+                    if (unfinished) {
+                        flurry();
+                    }
+                }
+            }, i);
+        }
     }
+
+    var unfinished = function() {
+        var noodle;
+            setTimeout(function(){
+                button = document.getElementsByClassName("touchable primary")[7];
+                if (typeof(button) != 'undefined' && button != null) {
+                    noodle = true;
+                } else {
+                    noodle = false;
+                }
+                return noodle;
+            }, 1000);
+    }
+
+    flurry();
+
+
+    function boolfunc() {return true;}
+    console.log("boolfunc",boolfunc);
 }
