97bd7aa1874038a1a38df035c6bd3752470efe33
Evan Sandhoefner <evansandhoefner@college.harvard.edu>
2015-12-24 01:07:19 -0800

    popup window behaves well - clears, repopuplates, understands user closure

--- sneaky/page.js
+++ sneaky/page.js
@@ -1,19 +1,25 @@
-function KeyPress(e) {
-      var evtobj = window.event? event : e
-      if (evtobj.keyCode == 77 && evtobj.ctrlKey) {
-        var messages = document.getElementsByClassName("snippet preview");
-        console.log(messages);
-          myWindow = window.open("", "Messages", "width=400, height=300");
-          // this works to clear window but then it doesn't repopulate...
-          // myWindow.document.documentElement.innerHTML = "";
-        for (i=0; i < messages.length; i++ ) {
-            myWindow.document.write("<p>" + messages[i].lastChild.innerText + "</p>");
-        }
+/*  problems:
+    can't close popup window/clear its contents (without making it unwritable for future)
+    emoji, mobile authors
+*/
 
-      
+function KeyPress(e) {
+    var evtobj = window.event ? event : e
+    if (evtobj.keyCode == 77 && evtobj.ctrlKey) {
+        var messages = document.getElementsByClassName("snippet preview");
+        var authors = document.getElementsByClassName("author");
+        if (typeof(myWindow) != "undefined") {
+            if (myWindow.self != null) {
+                var daddy = myWindow.self;
+                daddy.opener = myWindow.self;
+                daddy.close();
+            }
+        }
+        myWindow = window.open("", "Messages", "width=400, height=300");
+        for (i = 0; i < messages.length; i++) {
+            myWindow.document.write("<p>" + authors[i].innerText + ": " + messages[i].lastChild.innerHTML + "</p>");
+        }
     }
 }
 
 document.onkeydown = KeyPress;
-
-
