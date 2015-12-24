1683e89190974b949b1e7dc39cd0e919e9e38220
Evan Sandhoefner <evansandhoefner@college.harvard.edu>
2015-12-24 01:09:04 -0800

    style

--- sneaky/page.js
+++ sneaky/page.js
@@ -1,13 +1,15 @@
 /*  problems:
-    can't close popup window/clear its contents (without making it unwritable for future)
-    emoji, mobile authors
+    emoji
+    mobile authors
 */
 
 function KeyPress(e) {
     var evtobj = window.event ? event : e
+
     if (evtobj.keyCode == 77 && evtobj.ctrlKey) {
         var messages = document.getElementsByClassName("snippet preview");
         var authors = document.getElementsByClassName("author");
+
         if (typeof(myWindow) != "undefined") {
             if (myWindow.self != null) {
                 var daddy = myWindow.self;
@@ -15,7 +17,9 @@ function KeyPress(e) {
                 daddy.close();
             }
         }
+
         myWindow = window.open("", "Messages", "width=400, height=300");
+
         for (i = 0; i < messages.length; i++) {
             myWindow.document.write("<p>" + authors[i].innerText + ": " + messages[i].lastChild.innerHTML + "</p>");
         }
