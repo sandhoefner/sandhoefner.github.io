854118fa94a54732d0852da9b838f34f61861287
Evan Sandhoefner <evansandhoefner@gmail.com>
2016-07-14 15:00:25 +0200

    page.js is nicely commented

--- bestiario/qa_chrome/page.js
+++ bestiario/qa_chrome/page.js
@@ -4,14 +4,18 @@
 // ability for user to directly manipulate metadata
 // comments
 // very not error-proof for unusual use cases like filling but not posting
-// show time in addition to date in reporting metadata
+// jquery won't work if you turn on/reload extension after loading page
+
 $(document).ready(function() {
+    // determine whether we're on ask or answer page
     pageTitle = document.getElementsByClassName("page-title")[0].innerText;
     if (pageTitle == "Ask a question") {
-        // get data
+        // code for ask page
+        // get spreadsheet from google drive
         Papa.parse("http://docs.google.com/spreadsheets/u/0/d/1lTFyYrBbcDGqZeKCq3rrPUA7-HiztH0vj0Fnsd8MyJ0/export?format=csv&id=1lTFyYrBbcDGqZeKCq3rrPUA7-HiztH0vj0Fnsd8MyJ0&gid=1313316691", {
             download: true,
             complete: function(data) {
+                // get metadata from chrome extension storage
                 chrome.storage.sync.get('meta', function(result) {
                     if (!(result.meta)) {
                         alert("no metadata found; starting at row 1");
@@ -25,7 +29,8 @@ $(document).ready(function() {
                         console.log("metadata found; row " + result.meta + ". Metadata will update after you post.");
                         row = result.meta;
                     }
-                   
+                    
+                    // define constants
                     options = {
                         "Data Management": 33,
                         "Charts": 34,
@@ -39,48 +44,56 @@ $(document).ready(function() {
                         "Formulas": 42
                     };
 
-                    title_col = 1;
+                    valid_options = [33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
+
                     id_col = 0;
+                    title_col = 1;
                     desc_col = 2;
                     cat_col = 3;
                     tag_col = 4;
                     ans_col = 5;
 
-                    valid_options = [33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
+                    // extract useful content from spreadsheet
                     title_val = data.data[row][title_col];
                     category_raw = data.data[row][cat_col];
                     tag_raw = data.data[row][tag_col];
                     desc_val = data.data[row][desc_col];
 
+                    // clean raw content from spreadsheet
                     category_val = options[category_raw];
                     if (valid_options.indexOf(category_val) == -1) {
                         alert('Q&A Helper: "' + category_raw + '" is not a valid category. Kept default.');
                         category_val = 33;
                     }
 
+                    tags = tag_raw.split(',');
+
                     // get form elements
                     title = $("#title");
+
                     category = $("#category");
-                    tags = tag_raw.split(',');
+                    
                     tagHolder = $("#ap-tags-holder");
+
+                    ifrm = document.getElementById('description_ifr');
+                    ifrm = ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
+            
+                    // fill form
+                    ifrm.document.open();
+                    ifrm.document.write(desc_val);
+                    ifrm.document.close();
+
                     toAdd = "";
                     tags.forEach(function(tag) {
                         toAdd += "<input type='hidden' name='tags[]' value='" + tag + "'><p>" + tag + "</p>";
                     });
                     tagHolder[0].innerHTML += toAdd;
 
-                    // fill form
-                    ifrm = document.getElementById('description_ifr');
-                    ifrm = ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
-                    ifrm.document.open();
-                    ifrm.document.write(desc_val);
-                    ifrm.document.close();
-
                     title.val(title_val);
 
                     category.val(category_val);
 
-                    // error checks
+                    // spreadsheet content error checks
                     if (title_val.length < 10) {
                         alert("Q&A Helper: title must be at least 10 characters.");
                     }
@@ -91,15 +104,16 @@ $(document).ready(function() {
             }
         });
     } else {
-        // get data
+        // code for answer page
+        // get spreadsheet from google drive
         Papa.parse("http://docs.google.com/spreadsheets/u/0/d/1lTFyYrBbcDGqZeKCq3rrPUA7-HiztH0vj0Fnsd8MyJ0/export?format=csv&id=1lTFyYrBbcDGqZeKCq3rrPUA7-HiztH0vj0Fnsd8MyJ0&gid=1313316691", {
             download: true,
             complete: function(data) {
+                // get metadata from chrome extension storage, then increment row and save
                 chrome.storage.sync.get('meta', function(result) {
                     if (!(result.meta)) {
                         alert("error: no metadata found!");
                         row = NaN;
-
                     } else {
                         console.log("metadata found; row " + result.meta + ". Metadata updates now, so be sure to post!");
                         row = result.meta;
@@ -110,12 +124,16 @@ $(document).ready(function() {
                         });
                     }
 
+                    // define constants
                     id_col = 0;
                     ans_col = 5;
-
-                    post_id = data.data[row][id_col];
                     date = Date.now();
 
+                    // extract useful content from spreadsheet
+                    post_id = data.data[row][id_col];
+                    answer_val = data.data[row][ans_col];
+
+                    // update post history metadata
                     chrome.storage.sync.get('posted', function(result) {
                         if (!(result.posted)) {
                             toLoad = {};
@@ -135,15 +153,16 @@ $(document).ready(function() {
                         }
                     });
 
-                    // extract content
-                    answer_val = data.data[row][ans_col];
-
+                    // spreadsheet content error check
                     if (answer_val.length < 5) {
                         alert("Q&A Helper: answer must be at least 5 characters.");
                     }
 
+                    // get form element
                     ifrm = document.getElementById('description_ifr');
                     ifrm = ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
+
+                    // fill form
                     ifrm.document.open();
                     ifrm.document.write(answer_val);
                     ifrm.document.close();
