69312313028fd88154f9c9c0fbeb44418bfc410e
Evan Sandhoefner <evansandhoefner@gmail.com>
2016-08-04 17:35:28 +0200

    here have some code

--- spanish/parse_def.py
+++ spanish/parse_def.py
@@ -1,16 +1,37 @@
 from bs4 import BeautifulSoup
 import csv
-import os
 
-soup = BeautifulSoup(open("../../../spanish_data/1_de.html"), "html.parser")
-quickdef = soup.findAll("div", {"class": "quickdef"})[1]
-source = quickdef.find("div", {"class": "source"})
-source_text = source.find("h1", {"class": "source-text"})
-lang = quickdef.find("div", {"class": "lang"})
-el = lang.find("div", {"class": "el"})
-
-lexeme = source_text.text
-defn = el.text
-
-print "lexeme: " + lexeme
-print "definition: " + defn
+with open('CREA_truncated.csv', 'rb') as csvfile:
+	myReader = csv.reader(csvfile, delimiter = ',', quotechar = '"')
+	rows = list(myReader)
+	rankCol = 0
+	wordCol = 1
+	afreqCol = 2
+	nfreqCol = 3
+	counter = 1
+	for row in rows[1:500]:
+		word = row[wordCol].decode('latin1').strip()
+		soup = BeautifulSoup(open("../../../spanish_data/" + str(counter) + "_" + word + ".html"), "html.parser")
+		try:
+			quickdef = soup.findAll("div", {"class": "quickdef"})[1]
+			source = quickdef.find("div", {"class": "source"})
+			source_text = source.find("h1", {"class": "source-text"})
+			lang = quickdef.find("div", {"class": "lang"})
+			el = lang.find("div", {"class": "el"})
+			lexeme = source_text.text
+			defn = el.text
+		except (IndexError, AttributeError) as error:
+			try:
+				quickdef = soup.findAll("div", {"class": "quickdef"})[0]
+				source = quickdef.find("div", {"class": "source"})
+				source_text = source.find("h1", {"class": "source-text"})
+				lang = quickdef.find("div", {"class": "lang"})
+				el = lang.find("div", {"class": "el"})
+				lexeme = source_text.text
+				defn = el.text
+			except (IndexError, AttributeError) as error2:
+				lexeme = defn = "11896"
+		print "input: " + word
+		print "lexeme: " + lexeme
+		print "definition: " + defn + "\n"
+		counter += 1
