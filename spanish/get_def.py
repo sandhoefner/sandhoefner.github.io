35dd45b4d38574ded1c3821f8307fd7f708af761
Evan Sandhoefner <evansandhoefner@gmail.com>
2016-08-11 13:22:19 +0200

    modified get_def to use granada instead of spanishDict, and notes

--- spanish/get_def.py
+++ spanish/get_def.py
@@ -12,20 +12,21 @@ user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, lik
 headers = {'User-Agent': user_agent}
 
 # in test run on apartment wifi this averaged 1.24 seconds per page
-with open('CREA_total.csv', 'rb') as csvfile:
-	myReader = csv.reader(csvfile, delimiter = '\t', quotechar = '"')
+with open('CREA_truncated_utf8.csv', 'rb') as csvfile:
+	myReader = csv.reader(csvfile, delimiter = ',', quotechar = '"')
 	rows = list(myReader)
 	# cols: rank, word, abs_freq, norm_freq
 	rankCol = 0
 	wordCol = 1
 	afreqCol = 2
 	nfreqCol = 3
-	for row in rows[115917:140695]:
-		word = row[wordCol].decode('latin1').strip()
+	for row in rows:
+		word = row[wordCol].decode('utf-8').strip()
+		# print word
 		rank = row[rankCol]
-		url = "http://www.spanishdict.com/translate/" + word
+		url = "http://eubd1.ugr.es/xtf/search?lng=sp&sort=title&keyword=" + word
 		r = requests.get(url, headers=headers)
-		with open("/home/evan/Documents/spanish_data/"+rank+"_"+word+".html", "w+") as f:
-	  		f.write(r.content)
-	  		print "successfully wrote " + rank + " " + word
-	  	print "done"
+		with open("/home/evan/Documents/granada_data/"+rank+"_"+word+".html", "w+") as f:
+			f.write(r.content)
+			print "successfully wrote " + rank + " " + word
+	print "done"
