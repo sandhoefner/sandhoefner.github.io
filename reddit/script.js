44be39a396fbd2c38faeaefd8ddb115f43f28bdb
Evan Sandhoefner <evansandhoefner@gmail.com>
2016-08-23 17:07:07 +0200

    strong foundation

--- reddit/script.js
+++ reddit/script.js
@@ -1,4 +1,8 @@
 function auth() {
+	// oh I don't need to authorize right now
+	// I just needed to send get arguments with jquery rather than in url
+	// but I'll keep this code in case
+	//
 	// do something(?) with this for security and stuff
 	var state = Math.random().toString(36).substring(7);
 	console.log(state);
@@ -10,12 +14,33 @@ function auth() {
 				'state='+state+'&redirect_uri='+redir+'&duration='+dur+'&scope='+scope);
 }
 
+var pages = [];
+
 function submit() {
 	var subreddit = $("#subreddit").val();
-	var url = "https://www.reddit.com/r/" + subreddit + "/top/.json?limit=100?t=all";
-	$.get(url, function(data, status){
-		console.log("Data: ",data,"Status: ",status);
+	var url = "https://www.reddit.com/r/" + subreddit + "/top/.json";
+	$.get(url, { limit: '100', t: 'all' }, function(data) {
+		data.data.children.forEach(function(post) {
+			pages.push(post.data);
+		});
+		next(data.data.after, url);
 	});
 }
 
-// &after=t3_3zd9kd
+function next(after, url) {
+	if (!after) {
+		ajaxDone();
+	} else {
+		$.get(url, { limit: '100', t: 'all', after: after }, function(data) {
+			data.data.children.forEach(function(post) {
+				pages.push(post.data);
+			});
+			next(data.data.after, url);
+		});
+	}
+}
+
+function ajaxDone() {
+	console.log("no more posts");
+	console.log(pages);
+}
