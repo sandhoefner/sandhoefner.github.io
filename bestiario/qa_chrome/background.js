e1e07c8110755d8377cd883c9d335a7e9e5aecff
Evan Sandhoefner <evansandhoefner@gmail.com>
2016-09-07 22:02:15 -0400

    moved stuff for alberto to Reddit repo

--- bestiario/qa_chrome/background.js
+++
@@ -1,6 +0,0 @@
-// Called when the user clicks on the browser action.
-	chrome.browserAction.onClicked.addListener(function(tab) {
-	  // No tabs or host permissions needed!
-	  console.log('Filling form at ' + tab.url);
-	  chrome.tabs.executeScript(null, {file: "page.js"});
-	});
