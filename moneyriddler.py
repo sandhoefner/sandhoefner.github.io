65c2c2781a049c2d278867bc944ebe50ef813e0c
Evan Sandhoefner <evansandhoefner@gmail.com>
2016-09-10 16:20:08 -0400

    nope

--- moneyriddler.py
+++ moneyriddler.py
@@ -1,5 +1,9 @@
-from random import randint
+# run with python3
 
+from random import randint
+import statistics
+
+size = int(input())
 myPos = 3
 billPos = 3
 results = []
@@ -7,18 +11,18 @@ turns = []
 
 def movef(start, direc):
 	ret = start + direc
-	if ret == 6:
+	if ret > size:
 		ret = 1
 	elif ret == 0:
-		ret = 5
+		ret = size
 	return ret
 
 for i in range(100):
 	move = randint(-1,1)
+	turnCount = 1
 	if move == 0:
 		winPos = billPos
 	else:
-		turnCount = 1
 		while move != 0:
 			turnCount += 1
 			billPos = movef(billPos, move)
@@ -27,11 +31,14 @@ for i in range(100):
 	results.append(winPos)
 	turns.append(turnCount)
 
-counts = [0,0,0,0,0]
+counts = [0] * size
 for result in results:
 	counts[result-1] += 1
 
 for count in counts:
-	print float(count)/len(results)
+	print(float(count)/len(results))
 
-print turns
+print(turns)
+
+print("standard deviation of turns: " + str(statistics.stdev(turns)))
+print("average turns: " + str(statistics.mean(turns)))
