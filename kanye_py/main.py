cda2b2845b2b83647b9d0b63f725c8e385b1442d
Evan Sandhoefner <evansandhoefner@gmail.com>
2016-10-30 18:25:59 -0400

    some changes

--- kanye_py/main.py
+++ kanye_py/main.py
@@ -11,6 +11,8 @@ import cv2
 import sys
 import threading
 import glob
+import math
+from operator import itemgetter
 
 
 sys.path.append('/usr/local/lib/python2.7/site-packages')
@@ -103,6 +105,52 @@ def screenGrab(count):
 	return im
 	# im.save(os.getcwd() + '/caps/' + str(count) + '.png', 'PNG')
 
+
+# do I need to be doing float stuff all over this code or what
+
+# from http://stackoverflow.com/questions/8487893/generate-all-the-points-on-the-circumference-of-a-circle
+def pointsInCirc(r=85, n=256, c=295):
+	return [(math.cos(2*math.pi/n*x)*r+c,math.sin(2*math.pi/n*x)*r+c) for x in xrange(0,n)]
+
+# circumference is center of bumper, not perimeter of zone
+starting_circle = pointsInCirc()
+
+# step size is int for number of indices in array to jump
+# current bumper pos (x,y)
+# direction -1 or 1
+# returns tuple (left_pos, right_pos, warp_pos)
+def get_next_bumper_pos(current_bumper_pos,step_size):
+	global starting_circle
+	best_dist = 1000000
+	best_i = None
+	for (i,point) in enumerate(starting_circle):
+		validX = starting_circle[i][0]
+		validY = starting_circle[i][1]
+		seenX = current_bumper_pos[0]
+		seenY = current_bumper_pos[1]
+		print validX
+		print validY
+		print seenX
+		print seenY
+		# float?
+		# this is annoying for debugging
+		arg1, arg2 = validX - seenX, validY - seenY
+		arg3 = (arg1) ** 2 + (arg2) ** 2
+		dist = math.sqrt(arg3)
+		if dist < best_dist:
+			best_dist = dist
+			best_i = i
+	# be careful with corner cases here
+	next_i_left = (best_i - step_size)
+	next_i_right = (best_i + step_size)
+	next_i_space = (best_i + len(starting_circle) / 2)
+	return (starting_circle[next_i_left % len(starting_circle)],
+			starting_circle[next_i_right % len(starting_circle)],
+			starting_circle[next_i_space % len(starting_circle)])
+
+
+
+
 def play():
 	pyautogui.hotkey('winleft', '9')
 	time.sleep(0.5)
@@ -116,6 +164,10 @@ def play():
 	pyautogui.keyUp('right')
 	tick()
 
+def distance(point1, point2):
+	return math.sqrt(float(point1[0] - point2[0]) ** 2 + float(point1[1] - point2[1]) ** 2)
+
+
 def bestMove(oldPath, newPath):
 	if oldPath != None and newPath != None:
 		newLocations = getLocations(newPath)
@@ -128,51 +180,65 @@ def bestMove(oldPath, newPath):
 		print newPath
 		kanyeChange = [newLocations[0] - oldLocations[0], newLocations[1] - oldLocations[1]]
 		# is this even logical
-		lookAhead = 0
+		lookAhead = 0.5
 		kanyeNext = [newLocations[0] + lookAhead*kanyeChange[0], newLocations[1] + lookAhead*kanyeChange[1]]
 
 		# these might be too old by the time I'm making this decision???
 		bumperX = newLocations[2]
 		bumperY = newLocations[3]
 
-		# this could all be wrong
-		kanyeLeft, kanyeUp = True, True
-		if kanyeNext[0] - bumperX > 0:
-			kanyeLeft = False
-		if kanyeNext[1] - bumperY > 0:
-			kanyeUp = False
+		#  int parameter here should be 256 * percentage of circumference traveled per step, observationally
+		options = get_next_bumper_pos((bumperX, bumperY), int(256 * 0.17))
 
+		distances = [distance(p, kanyeNext) for p in options]
 
-		if bumperX < 295 and bumperY > 295:
-			bumperQuadrant = 1
-			# if kanyeLeft and not kanyeUp:
-			# 	return 'space'
-			if kanyeLeft or kanyeUp:
-				return 'left'
+		# from http://stackoverflow.com/questions/13300962/python-find-index-of-minimum-item-in-list-of-floats
+		best_idx = min(enumerate(distances), key=itemgetter(1))[0]
 
-		elif bumperX > 295 and bumperY > 295:
-			bumperQuadrant = 2
-			# if not kanyeLeft and not kanyeUp:
-			# 	return 'space'
-			if kanyeLeft or not kanyeUp:
-				return 'left'
+		named_options = ['left', 'right', 'space']
 
-		elif bumperX > 295 and bumperY < 295:
-			bumperQuadrant = 3
-			# if kanyeUp and not kanyeLeft:
-			# 	return 'space'
-			if kanyeLeft or kanyeUp:
-				return 'left'
-
-		elif bumperX > 295 and bumperY > 295:
-			bumperQuadrant = 4
-			# if kanyeLeft and kanyeUp:
-			# 	return 'space'
-			if kanyeLeft or not kanyeUp:
-				return 'left'
-
+		return named_options[best_idx]
 
 	return 'right'
+
+		# this could all be wrong
+		# kanyeLeft, kanyeUp = True, True
+		# if kanyeNext[0] - bumperX > 0:
+		# 	kanyeLeft = False
+		# if kanyeNext[1] - bumperY > 0:
+		# 	kanyeUp = False
+
+
+		# if bumperX < 295 and bumperY > 295:
+		# 	bumperQuadrant = 1
+		# 	# if kanyeLeft and not kanyeUp:
+		# 	# 	return 'space'
+		# 	if kanyeLeft or kanyeUp:
+		# 		return 'left'
+
+		# elif bumperX > 295 and bumperY > 295:
+		# 	bumperQuadrant = 2
+		# 	# if not kanyeLeft and not kanyeUp:
+		# 	# 	return 'space'
+		# 	if kanyeLeft or not kanyeUp:
+		# 		return 'left'
+
+		# elif bumperX > 295 and bumperY < 295:
+		# 	bumperQuadrant = 3
+		# 	# if kanyeUp and not kanyeLeft:
+		# 	# 	return 'space'
+		# 	if kanyeLeft or kanyeUp:
+		# 		return 'left'
+
+		# elif bumperX > 295 and bumperY > 295:
+		# 	bumperQuadrant = 4
+		# 	# if kanyeLeft and kanyeUp:
+		# 	# 	return 'space'
+		# 	if kanyeLeft or not kanyeUp:
+		# 		return 'left'
+
+
+
 
 
 # flutter
