025eb3d3303cdb9f242b9ef1d347f1184d7f5a47
Evan Sandhoefner <evansandhoefner@gmail.com>
2016-11-28 19:10:43 -0500

    0

---
+++ 6034/lab9/lab9.py
@@ -0,0 +1,72 @@
+# MIT 6.034 Lab 9: Boosting (Adaboost)
+# Written by Jessica Noss (jmn), Dylan Holmes (dxh), and 6.034 staff
+
+from math import log as ln
+from utils import *
+
+
+#### BOOSTING (ADABOOST) #######################################################
+
+def initialize_weights(training_points):
+    """Assigns every training point a weight equal to 1/N, where N is the number
+    of training points.  Returns a dictionary mapping points to weights."""
+    raise NotImplementedError
+
+def calculate_error_rates(point_to_weight, classifier_to_misclassified):
+    """Given a dictionary mapping training points to their weights, and another
+    dictionary mapping classifiers to the training points they misclassify,
+    returns a dictionary mapping classifiers to their error rates."""
+    raise NotImplementedError
+
+def pick_best_classifier(classifier_to_error_rate, use_smallest_error=True):
+    """Given a dictionary mapping classifiers to their error rates, returns the
+    best* classifier, or raises NoGoodClassifiersError if best* classifier has
+    error rate 1/2.  best* means 'smallest error rate' if use_smallest_error
+    is True, otherwise 'error rate furthest from 1/2'."""
+    raise NotImplementedError
+
+def calculate_voting_power(error_rate):
+    """Given a classifier's error rate (a number), returns the voting power
+    (aka alpha, or coefficient) for that classifier."""
+    raise NotImplementedError
+
+def get_overall_misclassifications(H, training_points, classifier_to_misclassified):
+    """Given an overall classifier H, a list of all training points, and a
+    dictionary mapping classifiers to the training points they misclassify,
+    returns a set containing the training points that H misclassifies.
+    H is represented as a list of (classifier, voting_power) tuples."""
+    raise NotImplementedError
+
+def is_good_enough(H, training_points, classifier_to_misclassified, mistake_tolerance=0):
+    """Given an overall classifier H, a list of all training points, a
+    dictionary mapping classifiers to the training points they misclassify, and
+    a mistake tolerance (the maximum number of allowed misclassifications),
+    returns False if H misclassifies more points than the tolerance allows,
+    otherwise True.  H is represented as a list of (classifier, voting_power)
+    tuples."""
+    raise NotImplementedError
+
+def update_weights(point_to_weight, misclassified_points, error_rate):
+    """Given a dictionary mapping training points to their old weights, a list
+    of training points misclassified by the current weak classifier, and the
+    error rate of the current weak classifier, returns a dictionary mapping
+    training points to their new weights.  This function is allowed (but not
+    required) to modify the input dictionary point_to_weight."""
+    raise NotImplementedError
+
+def adaboost(training_points, classifier_to_misclassified,
+             use_smallest_error=True, mistake_tolerance=0, max_rounds=INF):
+    """Performs the Adaboost algorithm for up to max_rounds rounds.
+    Returns the resulting overall classifier H, represented as a list of
+    (classifier, voting_power) tuples."""
+    raise NotImplementedError
+
+
+#### SURVEY ####################################################################
+
+NAME = 'Evan Sandhoefner'
+COLLABORATORS = 'Ryan Kerr'
+HOW_MANY_HOURS_THIS_LAB_TOOK = 8
+WHAT_I_FOUND_INTERESTING = 'Everything'
+WHAT_I_FOUND_BORING = 'Nothing'
+SUGGESTIONS = None
