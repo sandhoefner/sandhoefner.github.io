138e0c0cca00eef919a09b2fa559915e1e85b2f1
Evan Sandhoefner <evansandhoefner@gmail.com>
2018-11-08 04:27:03 +0000

    improved counter accuracy

--- new_tab_animal_kill_counter/script.js
+++ new_tab_animal_kill_counter/script.js
@@ -1,10 +1,13 @@
 console.log("ok");
 
-var period = 0.56321428571;
+// var period = 0.56321428571;
+// above is what it would be for 1, but JS granularity is insufficient
+// (using the 56 billion per year)
+var period = 18;
 
 function increment(total) {
 	document.getElementById("counter").innerText = total;
-    setTimeout(function() {increment(total + 1);}, period);
+    setTimeout(function() {increment(total + 32);}, period);
 }
 
 increment(0);
